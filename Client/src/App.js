34b0146977eac34f2b25fded98a940f2ff647708
Tanner <rooftb@email.sc.edu>
2019-10-08 16:30:01 -0400

    Create Client

---
+++ Client/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com/"
+});
+
+class App extends Component {
+  state = {
+    hello: null
+  };
+
+  componentDidMount() {
+    this.asyncFunction();
+  }
+
+  asyncFunction = async () => {
+    await fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(res => res.json())
+      .then(json => console.log(json));
+  };
+
+  render() {
+    return (
+      <div>{this.state.hello ? <div> {this.state.hello} </div> : null}</div>
+    );
+  }
+}
+
+export default App;
